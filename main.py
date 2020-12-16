5d214bf2daf5194a91e7eb898a0923d8e4abe29a
Zyoulou <Zyoulou_@outlook.fr>
2020-12-15 23:46:28 -0500

    Basic discord bot (ping command)

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from config import discordToken, osuKey
 import discord
+from discord.ext import commands
 import osuapi
 
-print("hello")
+description = '''ex'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+# help_command=None
+bot = commands.Bot(command_prefix='.', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+
+bot.run(discordToken)
